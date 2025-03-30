d9ce03b59dae2027503ecad2c80de8d5accf8ca4
Dino-code94 Klaric <gya29x32@students.codeinstitute.net>
2025-03-30 20:05:56 +0000

    Adding questions and correct answers
    git add .

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const questions = [
+  {
+    question: "What is the title of Green Day's album released in 2004?",
+    correctAnswer: "American Idiot"
+  },
+  {
+    question: "Which Green Day song includes the lyrics 'I walk a lonely road, the only one that I have ever known'?",
+    correctAnswer: "Boulevard of Broken Dreams"
+  },
+  {
+    question: "What is Green Day's genre?",
+    correctAnswer: "Punk rock"
+  },
+  {
+    question: "What year was Green Day formed?",
+    correctAnswer: "1987"
+  },
+  {
+    question: "Which member of Green Day is the lead vocalist and guitarist?",
+    correctAnswer: "Billie Joe Armstrong"
+  },
+  {
+    question: "What is the name of Green Day's debut album?",
+    correctAnswer: "39/Smooth"
+  },
+  {
+    question: "Which Green Day album features the song 'Wake Me Up When September Ends'?",
+    correctAnswer: "American Idiot"
+  },
+  {
+    question: "In which city was Green Day formed?",
+    correctAnswer: "Oakland"
+  },
+  {
+    question: "What Green Day album was released in 2009?",
+    correctAnswer: "21st Century Breakdown"
+  },
+  {
+    question: "Which Green Day song was used as the theme for the 'American Idiot' musical?",
+    correctAnswer: "American Idiot"
+  }
+];
